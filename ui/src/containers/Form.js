e78703476631456055c4d6cbffdc52b138ff0a9a
nisten <nisten@outlook.com>
2019-03-05 08:32:33 -0500

    get map button and ip api call

--- ui/src/containers/Form.js
+++ ui/src/containers/Form.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
+const ipToken = '29b43a4e7b0057'
+
+
 class Form extends React.Component {
+    ipLocation = async (e) => {
+        e.preventDefault()
+        const api_call = await fetch(`https://ipinfo.io?token=${ipToken}`)
+        const data = api_call.json()
+        console.log(data.country)
+    }
     render() {
         return (
             <form>
-                <input type="text" name="city" placeholder="City..."/>
-                <input type="text" name="country" placeholder="Country..."/>
-                <button>Get City</button>
+                <input type="text" name="city" placeholder="Enter an IP address..."/>
+                <button>Get Map</button>
             </form>
         )
     }
--- ui/src/pages/index.js
+++ ui/src/pages/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { withSiteData } from 'react-static'
 import Form from '../containers/Form.js'
 
-const accessToken = 'pk.eyJ1IjoibmlzdGVuIiwiYSI6ImNqcnlieXd0MjB3bjk0M25yb280Nm9kczQifQ.N3Fa7L770MHqp-AV3f9hoA'
+const mapToken = 'pk.eyJ1IjoibmlzdGVuIiwiYSI6ImNqcnlieXd0MjB3bjk0M25yb280Nm9kczQifQ.N3Fa7L770MHqp-AV3f9hoA'
 const ipCoordinates = '-79.3623,43.6776'
+
 
 export default withSiteData(() => (
   <div style={{ textAlign: 'center' }}>
     <h1>Welcome to static maps with server side rendering</h1>
     <Form />
-    <img src={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/static/${ipCoordinates},10,0,30/600x600@2x?access_token=${accessToken}`} />
+    <img src={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/static/${ipCoordinates},10,0,30/600x600@2x?access_token=${mapToken}`} />
   </div>
 ))
