fb7019eb66c2ae659ec1d4ce679f0b1cacb94b47
nisten <nisten@outlook.com>
2019-03-05 03:04:08 -0500

    static map image showing on first screen

---
+++ ui/src/containers/Form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Form extends React.Component {
+    render() {
+        return (
+            <form>
+                <input type="text" name="city" placeholder="City..."/>
+                <input type="text" name="country" placeholder="Country..."/>
+                <button>Get City</button>
+            </form>
+        )
+    }
+}
+
+export default Form
